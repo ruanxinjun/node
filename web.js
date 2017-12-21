dbfb2612ed2e9ff1a3f6683f229ad4bbd84d4155
ruanxinjun <83932134@qq.com>
2017-12-21 21:29:19 +0800

    1

--- web.js
+++ web.js
@@ -1,24 +1,42 @@
 var http = require('http');
 var fs = require('fs');
-var url =  require('url')
+var url =  require('url');
+var path = require('path');
 
 
 var server = http.createServer((req,rep)=>{
 	var pathname = url.parse(req.url).pathname;
-	if(req.url=="/"){
-		pathname="./static/index.html";
+	if(req.url=="/favicon.ico"){
+		return;
 	}
-	console.log(pathname);
-	fs.readFile('./static'+pathname,(error,data)=>{
+	if(req.url=="/"){
+		pathname="index.html";
+	};
+	let extname = path.extname(pathname).replace('.','');
+	fs.readFile('./static/'+pathname,(error,data)=>{
 		if(error){
 			throw error;
 		}else{
+			var mime = fs.readFileSync('./mime.json');
+			var js = JSON.parse(mime.toString().replace(/\./g,''));
+			var type = eval('js.'+extname);
+			console.log(type);
+			rep.writeHead(200,{"Content-type":type});
 			rep.end(data);
 		}
-
+		
 	});
-	
 });
 
+let getMime = (extname)=>{
+	switch(extname) {
+		case '.html':
+			return 'text/html';
+			break;
+		case'.jpg':
+			return 'image/jpg';
+			break;
+	}
+};
 
 server.listen(3000);
