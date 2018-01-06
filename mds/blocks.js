c1e1ba312edda10ee6671e2245b52d0c2eb436c2
ruanxinjun <83932134@qq.com>
2018-01-06 13:56:15 +0800

    1

---
+++ mds/blocks.js
@@ -0,0 +1,6 @@
+let fns = {
+	show1:function(){return 1},
+	show2:function(){return 2},
+	show3:function(){return 3}
+}; 
+module.exports = fns;
---
+++ mds/main.js
@@ -0,0 +1,5 @@
+let fns = require('./blocks');
+
+console.log(fns.show1());
+console.log(fns.show2());
+console.log(fns.show3());
